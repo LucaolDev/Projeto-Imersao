e901ab9cf0244cd3f2aa5c9e8f82dee120de9be8
Luis Carlos <133696936+LucaolDev@users.noreply.github.com>
2024-09-06 14:49:12 -0300

    Add files via upload

---
+++ Projeto-imersão/README.md
@@ -0,0 +1,26 @@
+Como usar?
+
+1. Digite o nome da linguagem que você quer conhecer no campo de pesquisa.
+
+2. Aperte em "Pesquisar" e pronto!
+
+3. Uma lista de informações sobre a linguagem aparecerá, incluindo:
+
+*Descrição: Descubra para que serve essa linguagem.
+
+*Ano de Criação: Saiba quando essa linguagem surgiu.
+
+*Paradigmas: Conheça as filosofias de programação que a linguagem utiliza.
+
+*Uso Principal: Entenda onde essa linguagem é mais utilizada.
+
+*Link: Mergulhe mais fundo e explore a linguagem em sua fonte original.
+
+O que você encontra aqui?
+
+*Um catálogo de linguagens: Java, Python, C++, HTML, CSS, Javascript... 
+um banquete de conhecimento para programadores de todos os níveis!
+
+*Uma busca fácil: Encontre a linguagem que você procura com apenas um clique.
+
+*Informações detalhadas: Descubra tudo sobre a linguagem de forma clara e organizada.
---
+++ Projeto-imersão/dados.js
@@ -0,0 +1,93 @@
+let dados = [
+    {
+        titulo: "Java",
+        descricao: "Linguagem de programação orientada a objetos de propósito geral, conhecida por sua portabilidade e robustez, amplamente utilizada no desenvolvimento de aplicações empresariais e Android.",
+        anoCriacao: 1995,
+        paradigmas: ["orientada a objetos"],
+        usoPrincipal: "Desenvolvimento de aplicações empresariais, desenvolvimento Android, desenvolvimento de jogos, big data",
+        link: "https://pt.wikipedia.org/wiki/Java_(linguagem_de_programa%C3%A7%C3%A3o)",
+        tags: "backend mobile"
+    },
+    {
+        titulo: "Python",
+        descricao: "Python é uma linguagem de programação de alto nível, interpretada, de propósito geral e de tipagem dinâmica, famosa por sua sintaxe clara e legível.",
+        anoCriacao: 1991,
+        paradigmas: ["orientada a objetos", "funcional", "imperativa"],
+        usoPrincipal: "Data Science, Machine Learning, Desenvolvimento web, Automação",
+        link: "https://www.python.org/",
+        tags: "backend machine-learning data-science"
+    },
+    {
+        titulo: "HTML",
+        descricao: "Linguagem de marcação utilizada para estruturar e apresentar conteúdo na Web.",
+        anoCriacao: 1993,
+        paradigmas: ["declarativa"],
+        usoPrincipal: "Estrutura de páginas web",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML",
+        tags: "web frontend"
+    },
+    {
+        titulo: "CSS",
+        descricao: "Linguagem de estilo utilizada para descrever a apresentação de um documento HTML.",
+        anoCriacao: 1996,
+        paradigmas: ["declarativa"],
+        usoPrincipal: "Estilização de páginas web",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
+        tags: "web frontend"
+    },
+    {
+        titulo: "JavaScript",
+        descricao: "Linguagem de programação interpretada, cliente-lado, utilizada para criar interatividade em páginas web.",
+        anoCriacao: 1995,
+        paradigmas: ["orientada a objetos", "funcional", "imperativa"],
+        usoPrincipal: "Desenvolvimento frontend, desenvolvimento backend (Node.js), desenvolvimento de jogos, aplicativos móveis",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+        tags: "backend frontend mobile"
+    },
+    {
+        titulo: "SQL",
+        descricao: "Linguagem de consulta estruturada para gerenciar bancos de dados relacionais.",
+        anoCriacao: "1970",
+        paradigmas: ["declarativa"],
+        usoPrincipal: "Gerenciamento de bancos de dados relacionais",
+        link: "https://www.w3schools.com/sql/",
+        tags: "backend database"
+    },
+    {
+        titulo: "C",
+        descricao: "Linguagem de programação procedural de baixo nível, conhecida por sua eficiência e portabilidade.",
+        anoCriacao: "1972",
+        paradigmas: ["imperativa", "procedural"],
+        usoPrincipal: "Desenvolvimento de sistemas operacionais, drivers de dispositivos, aplicações de tempo real, embedded systems",
+        link: "https://en.wikipedia.org/wiki/C_(programming_language)",
+        tags: "sistemas baixo-nível procedural c++"
+    },
+    {
+        titulo: "C++",
+        descricao: "Linguagem de programação multiparadigma, que adiciona recursos de programação orientada a objetos ao C.",
+        anoCriacao: "1983",
+        paradigmas: ["imperativa", "procedural", "orientada a objetos"],
+        usoPrincipal: "Desenvolvimento de jogos, aplicativos de alto desempenho, desenvolvimento de sistemas, bibliotecas padrão",
+        link: "https://en.wikipedia.org/wiki/C++",
+        tags: "backend c orientada a objetos alto-desempenho sistemas"
+    },
+    {
+        titulo: "C#",
+        descricao: "Linguagem de programação orientada a objetos, desenvolvida pela Microsoft para a plataforma .NET.",
+        anoCriacao: "2000",
+        paradigmas: ["orientada a objetos"],
+        usoPrincipal: "Desenvolvimento de aplicações Windows, desenvolvimento web (ASP.NET), desenvolvimento de jogos (Unity), desenvolvimento mobile",
+        link: "https://docs.microsoft.com/en-us/dotnet/csharp/",
+        tags: "microsoft .net orientada a objetos windows jogos"
+    },
+    {
+        titulo: "PHP",
+        descricao: "Linguagem de script interpretada, especialmente adequada para o desenvolvimento web, conhecida por sua sintaxe semelhante ao C.",
+        anoCriacao: "1994",
+        paradigmas: ["imperativa", "orientada a objetos"],
+        usoPrincipal: "Desenvolvimento web (backend), CMS (WordPress, Drupal), frameworks (Laravel, Symfony)",
+        link: "https://www.php.net/",
+        tags: "web backend servidor"
+    }
+
+];
